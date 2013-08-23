396dc9d5017031b88b77e8b4caf545d47dccfe06
Yomguithereal <guillaumeplique@msn.com>
2013-08-24 00:31:49 +0200

    Adding a style arity to color function

--- src/mastermind/colors.clj
+++ src/mastermind/colors.clj
@@ -9,11 +9,14 @@
 (ns mastermind.colors)
 
 (def __colors (zipmap [:black :red :green :yellow :blue :purple :cyan :white] (range 0 8)))
+(def __styles (zipmap [:reset :bold :dim :unknown :underline :blink :reverse :hidden] (range 0 9)))
 
 (defn color
     "Return an ANSI colored string"
-    [string color]
-    (str "\033[3" (__colors color) "m" string "\033[0m"))
+    ([string color]
+    	(str "\033[3" (__colors color) "m" string "\033[0m"))
+    ([string color style]
+    	(str "\033[" (__styles style) ";3" (__colors color) "m" string "\033[0m")))
 
 (defn background-color
     "Return an ANSI background colored string"
